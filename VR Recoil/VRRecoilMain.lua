87bb148c7b3593421cc9cb1536bc7d764b1fbefe
Hugo Zink <hugo.zink@hotmail.com>
2020-07-15 16:06:05 +0200

    Slightly increased recoil when not using IreNFist

--- VR Recoil/VRRecoilMain.lua
+++ VR Recoil/VRRecoilMain.lua
@@ -3,8 +3,8 @@ if not VRRecoil then
 
 	-- Tweakdata
 	VRRecoil.Tweak = {}
-	VRRecoil.Tweak.OnehandedRecoilMultiplier = 0.65
-	VRRecoil.Tweak.TwohandedRecoilMultiplier = 0.3
+	VRRecoil.Tweak.OnehandedRecoilMultiplier = 0.75
+	VRRecoil.Tweak.TwohandedRecoilMultiplier = 0.35
 	
 	-- User settings
 	VRRecoil.Settings = {
@@ -14,4 +14,13 @@ if not VRRecoil then
 	-- Is the currently fired weapon the akimbo one?
 	-- This variable is updated on a PreHook every time the user fires weapons in VR
 	VRRecoil.FiredWeaponIsAkimbo = false
+
+	-- Blatant favouritism inbound
+	-- If Irenfist is installed, loosen up on the recoil just slightly.
+	-- IreNFist has brutal recoil and it needs to be reduced just slightly more in VR to be fun.
+	if BeardLib.Utils:ModLoaded("irenfist") or BeardLib.Utils:ModLoaded("infmod") then
+		VRRecoil.Tweak.OnehandedRecoilMultiplier = 0.65
+		VRRecoil.Tweak.TwohandedRecoilMultiplier = 0.3
+		log("[VR Recoil] IreNFist found, recoil multipliers slightly lowered")
+	end
 end
