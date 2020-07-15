54c8e684194a8f5cf7537f5f59724fcb805035b3
Hugo Zink <hugo.zink@hotmail.com>
2020-07-15 16:46:34 +0200

    Added proper IreNFist compatibility

--- VR Recoil/VRRecoilMain.lua
+++ VR Recoil/VRRecoilMain.lua
@@ -15,12 +15,15 @@ if not VRRecoil then
 	-- This variable is updated on a PreHook every time the user fires weapons in VR
 	VRRecoil.FiredWeaponIsAkimbo = false
 
+	VRRecoil.ModCompatibility = {}
+
 	-- Blatant favouritism inbound
 	-- If Irenfist is installed, loosen up on the recoil just slightly.
 	-- IreNFist has brutal recoil and it needs to be reduced just slightly more in VR to be fun.
-	if BeardLib.Utils:ModLoaded("irenfist") or BeardLib.Utils:ModLoaded("infmod") then
+	if BeardLib.Utils:ModLoaded("irenfist") or BeardLib.Utils:ModLoaded("infmod") or BeardLib.Utils:ModLoaded("IRE AND FIST REBORN") then
 		VRRecoil.Tweak.OnehandedRecoilMultiplier = 0.65
 		VRRecoil.Tweak.TwohandedRecoilMultiplier = 0.3
+		VRRecoil.ModCompatibility.IreNFist = true
 		log("[VR Recoil] IreNFist found, recoil multipliers slightly lowered")
 	end
 end
--- VR Recoil/fpcameraplayerbase.lua
+++ VR Recoil/fpcameraplayerbase.lua
@@ -68,17 +68,69 @@ function FPCameraPlayerBase:recoil_kick(up, down, left, right)
     -- If the fired weapon is not the akimbo one, return the original function
     if not VRRecoil.FiredWeaponIsAkimbo then
         return camera_recoil_kick_orig(self, up, down, left, right)
-    end
-
-    -- Below is the same as the original function, except modified for akimbos.
-
-	if math.abs(self._recoil_kick_akimbo.accumulated) < 20 then
-		local v = math.lerp(up, down, math.random())
-		self._recoil_kick_akimbo.accumulated = (self._recoil_kick_akimbo.accumulated or 0) + v
 	end
+	
+	-- IreNFist compatibility for the offhand akimbo, copypasted from IreNFist itself
+	if VRRecoil.ModCompatibility.IreNFist then
+		-- If we get this far, this means we already know that the fired weapon is akimbo.
+		-- Technically this means that the same recoil "pattern" will run over both weapons at once (instead of individually), but I consider this acceptable.
 
-	local h = math.lerp(left, right, math.random())
-	self._recoil_kick_akimbo.h.accumulated = (self._recoil_kick_akimbo.h.accumulated or 0) + h
+		-- set default recoil table
+		local recoil_table = {
+			{-1, -1, 0, 0}
+		}
+		local recoil_loop_point = 999
+
+		-- get recoil table
+		if self._parent_unit then
+			if self._parent_unit:inventory() then
+				if self._parent_unit:inventory():equipped_unit() then
+					if self._parent_unit:inventory():equipped_unit():base() then
+						if self._parent_unit:inventory():equipped_unit():base()._recoil_table then
+							recoil_table = self._parent_unit:inventory():equipped_unit():base()._recoil_table
+							recoil_loop_point = self._parent_unit:inventory():equipped_unit():base()._recoil_loop_point or 999
+						end
+					end
+				end
+			end
+		end
+
+		local recoil_index = self._accumulated_recoil+1
+		-- don't pull table values that are off the table
+		if recoil_index > #recoil_table then
+			-- invalid or no recoil loop point
+			if (recoil_loop_point or 0) > #recoil_table then
+				recoil_index = #recoil_table
+			-- use loop point
+			else
+				recoil_index = recoil_loop_point or #recoil_table
+				self._accumulated_recoil = recoil_loop_point - 1
+			end
+		end
+
+		-- send kick values
+		local v = math.lerp(up * recoil_table[recoil_index][1], down * recoil_table[recoil_index][2], math.random())
+		self._recoil_kick_akimbo.accumulated = (self._recoil_kick_akimbo.accumulated or 0) + v
+
+		local h = math.lerp(left * recoil_table[recoil_index][3], right * recoil_table[recoil_index][4], math.random())
+		self._recoil_kick_akimbo.h.accumulated = (self._recoil_kick_akimbo.h.accumulated or 0) + h
+
+		-- set InF recoil system values
+		self._last_shot_time = os.clock()
+		self._last_unapplied_recoil_time = self._last_unapplied_recoil_time or self._last_shot_time
+		self._current_wpn = self._parent_unit:inventory():equipped_unit():base()._factory_id
+		self._accumulated_recoil = self._accumulated_recoil + 1
+	else
+		-- Below is the same as the original function, except modified for akimbos.
+
+		if math.abs(self._recoil_kick_akimbo.accumulated) < 20 then
+			local v = math.lerp(up, down, math.random())
+			self._recoil_kick_akimbo.accumulated = (self._recoil_kick_akimbo.accumulated or 0) + v
+		end
+
+		local h = math.lerp(left, right, math.random())
+		self._recoil_kick_akimbo.h.accumulated = (self._recoil_kick_akimbo.h.accumulated or 0) + h
+	end
 end
 
 -- Update recoil values to allow gradual resetting of recoil for the akimbo weapon too
